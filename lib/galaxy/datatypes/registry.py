5cdab5a11237ab9df55130e0b2419152602227aa
Sveinung Gundersen <sveinugu@gmail.com>
2023-06-30 01:06:02 +0200

    Galaxy bugfix for binary datatypes with only sniff_prefix(), not sniff()

--- lib/galaxy/datatypes/registry.py
+++ lib/galaxy/datatypes/registry.py
@@ -614,7 +614,8 @@ class Registry:
 
     def is_extension_unsniffable_binary(self, ext):
         datatype = self.get_datatype_by_extension(ext)
-        return datatype is not None and isinstance(datatype, binary.Binary) and not hasattr(datatype, "sniff")
+        return datatype is not None and isinstance(datatype, binary.Binary) and \
+            not any(hasattr(datatype, attr) for attr in ('sniff', 'sniff_prefix'))
 
     def get_datatype_class_by_name(self, name):
         """
