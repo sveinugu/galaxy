fcca63f50faec37fed0ba52446b0e19c4ee9ec25
Sveinung Gundersen <sveinugu@gmail.com>
2023-06-30 00:08:16 +0200

    Refactored test_crypt4gh

--- test/unit/data/datatypes/test_crypt4gh.py
+++ test/unit/data/datatypes/test_crypt4gh.py
@@ -1,109 +1,143 @@
-import pytest
+from contextlib import contextmanager
 
+import pytest
+from typing import NamedTuple, Optional, Generator
+
+from galaxy.datatypes import sniff
 from galaxy.datatypes.binary import Crypt4ghEncryptedArchive
+from galaxy.datatypes.protocols import DatasetProtocol
+
 from .util import (
-    get_dataset,
-    get_input_files,
-    get_tmp_path, MockDataset, MockDatasetDataset, MockMetadata,
+    get_input_files, MockDataset, MockDatasetDataset, MockMetadata,
 )
 
-def test_crypt4gh_peek():
-    c4gh_loader = Crypt4ghEncryptedArchive()
-    with get_input_files("data-complete.c4gh") as input_files:
-        dataset = MockDataset(1)
-        dataset.file_name = input_files[0]
+ORIG_HEADER_SHA256 = "0491065aa0f4245a7a958fdefc487b9b31327ad594e009ea01d5f8b6b1bb3d8a"
+RECRYPTED_HEADER_SHA256 = "b15c077a3f07f970a04f57c4204fe756307f73b309c922a230696b793d12eee6"
+
+InputFileInfo = NamedTuple("InputFileInfo", [('dataset', DatasetProtocol),
+                                             ('file_prefix', sniff.FilePrefix),
+                                             ('contents', Optional[bytes])])
+
+@contextmanager
+def _get_input_file_info(file_name: str, dataset_id: int, read_contents: bool) -> Generator[InputFileInfo, None, None]:
+    with get_input_files(file_name) as input_files:
+        input_file_path = input_files[0]
+
+        dataset = MockDataset(dataset_id)
+        dataset.file_name = input_file_path
         dataset.dataset = MockDatasetDataset(dataset.file_name)
+        dataset.metadata = MockMetadata()
+
+        file_prefix = sniff.FilePrefix(input_file_path)
+
+        if read_contents:
+            with open(input_file_path, "rb") as input_file:
+                contents = input_file.read()
+        else:
+            contents = None
+
+        yield InputFileInfo(dataset=dataset, file_prefix=file_prefix, contents=contents)  # type:ignore
+
+
+@pytest.fixture
+def c4gh_loader() -> Crypt4ghEncryptedArchive:
+    return Crypt4ghEncryptedArchive()
+
+
+@pytest.fixture
+def c4gh_data_complete() -> Generator[InputFileInfo, None, None]:
+    return _get_input_file_info("data-complete.c4gh", 1, read_contents=False)
+
+
+@pytest.fixture
+def c4gh_data_header() -> Generator[InputFileInfo, None, None]:
+    return _get_input_file_info("data-header.c4gh", 2, read_contents=True)
+
+
+@pytest.fixture
+def c4gh_data_payload() -> Generator[InputFileInfo, None, None]:
+    return _get_input_file_info("data-payload.c4gh", 3, read_contents=True)
+
+
+@pytest.fixture
+def c4gh_data_header_recrypted() -> Generator[InputFileInfo, None, None]:
+    return _get_input_file_info("data-header-recrypted.c4gh", 4, read_contents=True)
+
+
+def test_crypt4gh_peek(c4gh_loader, c4gh_data_complete):
+    with c4gh_data_complete as data_complete:
+        dataset = data_complete.dataset
         c4gh_loader.set_peek(dataset)
         assert dataset.peek == "Crypt4GH encrypted dataset"
         assert dataset.blurb == "2.8 KB"
 
 
-def test_crypt4gh_sniff():
-    c4gh_loader = Crypt4ghEncryptedArchive()
+def test_crypt4gh_sniff_prefix(c4gh_loader, c4gh_data_complete, c4gh_data_header, c4gh_data_payload):
+    with c4gh_data_complete as data_complete:
+        assert c4gh_loader.sniff_prefix(data_complete.file_prefix) is True
 
-    input_filenames = ["data-complete.c4gh", "data-header.c4gh", "data-payload.c4gh"]
-    with get_input_files(*input_filenames) as input_files:
-        c4gh_complete_path, c4gh_header_path, c4gh_payload_path = input_files
-        assert c4gh_loader.sniff(c4gh_complete_path) is True
-        assert c4gh_loader.sniff(c4gh_header_path) is True
-        assert c4gh_loader.sniff(c4gh_payload_path) is False
+    with c4gh_data_header as data_header:
+        assert c4gh_loader.sniff_prefix(data_header.file_prefix) is False
 
-
-def _metadata_field_is_set(dataset, field_name):
-    return hasattr(dataset.metadata, field_name) and getattr(dataset.metadata, field_name) is not None
+    with c4gh_data_payload as data_payload:
+        assert c4gh_loader.sniff_prefix(data_payload.file_prefix) is False
 
 
-def test_crypt4gh_set_meta_not_crypt4gh_header():
-    c4gh_loader = Crypt4ghEncryptedArchive()
 
-    input_filenames = ["data-payload.c4gh", "data-complete.c4gh"]
-    with get_input_files(*input_filenames) as input_files:
-        c4gh_payload_path, c4gh_complete_path = input_files
+def test_crypt4gh_set_meta(c4gh_loader, c4gh_data_complete, c4gh_data_header, c4gh_data_header_recrypted):
+    with c4gh_data_complete as data_complete:
+        dataset = data_complete.dataset
 
-        dataset = MockDataset(1)
-        dataset.file_name = c4gh_payload_path
-        dataset.metadata = MockMetadata()
+        with c4gh_data_header as data_header:
+            c4gh_loader.set_meta(dataset=dataset)
 
-        c4gh_loader.set_meta(dataset=dataset)
+            assert dataset.metadata.crypt4gh_header == data_header.contents
+            assert dataset.metadata.crypt4gh_metadata_header_sha256 == ORIG_HEADER_SHA256
+            assert dataset.metadata.crypt4gh_dataset_header_sha256 == ORIG_HEADER_SHA256
+            assert not dataset.metadata.crypt4gh_compute_node_keypair_id
 
-        assert not _metadata_field_is_set(dataset, "crypt4gh_header")
-        assert not _metadata_field_is_set(dataset, "crypt4gh_metadata_header_sha256")
-        assert not _metadata_field_is_set(dataset, "crypt4gh_compute_node_keypair_id")
+        with c4gh_data_header_recrypted as data_header_recrypted:
+            c4gh_loader.set_meta(dataset=dataset, crypt4gh_header=data_header_recrypted.contents)
 
-        dataset.file_name = c4gh_complete_path
-        c4gh_loader.set_meta(dataset=dataset)
-
-        assert _metadata_field_is_set(dataset, "crypt4gh_header")
-        assert _metadata_field_is_set(dataset, "crypt4gh_metadata_header_sha256")
-        assert not _metadata_field_is_set(dataset, "crypt4gh_compute_node_keypair_id")
-
-        c4gh_loader.set_meta(dataset=dataset, crypt4gh_compute_node_keypair_id='cn_keypair_id_123')
-
-        assert _metadata_field_is_set(dataset, "crypt4gh_header")
-        assert _metadata_field_is_set(dataset, "crypt4gh_metadata_header_sha256")
-        assert _metadata_field_is_set(dataset, "crypt4gh_compute_node_keypair_id")
-
-        with open(c4gh_payload_path, "rb") as payload_file:
-            c4gh_loader.set_meta(dataset=dataset, crypt4gh_header=payload_file.read())
-
-        assert not _metadata_field_is_set(dataset, "crypt4gh_header")
-        assert not _metadata_field_is_set(dataset, "crypt4gh_metadata_header_sha256")
-        assert not _metadata_field_is_set(dataset, "crypt4gh_compute_node_keypair_id")
-
-
-def test_crypt4gh_set_meta():
-    c4gh_loader = Crypt4ghEncryptedArchive()
-
-    ORIG_HEADER_SHA256 = "0491065aa0f4245a7a958fdefc487b9b31327ad594e009ea01d5f8b6b1bb3d8a"
-    RECRYPTED_HEADER_SHA256 = "b15c077a3f07f970a04f57c4204fe756307f73b309c922a230696b793d12eee6"
-
-    input_filenames = ["data-complete.c4gh", "data-header.c4gh", "data-header-recrypted.c4gh"]
-    with get_input_files(*input_filenames) as input_files:
-        c4gh_complete_path, c4gh_orig_header_path, c4gh_recrypted_header_path = input_files
-
-        with open(c4gh_orig_header_path, "rb") as c4gh_orig_header_file:
-            c4gh_orig_header = c4gh_orig_header_file.read()
-
-        with open(c4gh_recrypted_header_path, "rb") as c4gh_recrypted_header_file:
-            c4gh_recrypted_header = c4gh_recrypted_header_file.read()
-
-        dataset = MockDataset(1)
-        dataset.file_name = input_files[0]
-        dataset.metadata = MockMetadata()
-
-        c4gh_loader.set_meta(dataset=dataset)
-
-        assert dataset.metadata.crypt4gh_header == c4gh_orig_header
-        assert dataset.metadata.crypt4gh_metadata_header_sha256 == ORIG_HEADER_SHA256
-        assert dataset.metadata.crypt4gh_dataset_header_sha256 == ORIG_HEADER_SHA256
-        assert not _metadata_field_is_set(dataset, 'crypt4gh_compute_node_keypair_id')
-
-        c4gh_loader.set_meta(dataset=dataset, crypt4gh_header=c4gh_recrypted_header)
-
-        assert dataset.metadata.crypt4gh_header == c4gh_recrypted_header
-        assert dataset.metadata.crypt4gh_metadata_header_sha256 == RECRYPTED_HEADER_SHA256
-        assert dataset.metadata.crypt4gh_dataset_header_sha256 == ORIG_HEADER_SHA256
-        assert not _metadata_field_is_set(dataset, 'crypt4gh_compute_node_keypair_id')
+            assert dataset.metadata.crypt4gh_header == data_header_recrypted.contents
+            assert dataset.metadata.crypt4gh_metadata_header_sha256 == RECRYPTED_HEADER_SHA256
+            assert dataset.metadata.crypt4gh_dataset_header_sha256 == ORIG_HEADER_SHA256
+            assert not dataset.metadata.crypt4gh_compute_node_keypair_id
 
         c4gh_loader.set_meta(dataset=dataset, crypt4gh_compute_node_keypair_id='cn_keypair_id_123')
         assert dataset.metadata.crypt4gh_compute_node_keypair_id == 'cn_keypair_id_123'
+
+
+def test_crypt4gh_set_meta_not_crypt4gh_data(c4gh_loader, c4gh_data_payload, c4gh_data_header):
+    with c4gh_data_payload as data_payload:
+        with pytest.raises(ValueError):
+            c4gh_loader.set_meta(dataset=data_payload.dataset)
+        _assert_c4gh_metadata_not_set(data_payload.dataset)
+
+    with c4gh_data_header as data_header:
+        with pytest.raises(ValueError):
+            c4gh_loader.set_meta(dataset=data_header.dataset)
+        _assert_c4gh_metadata_not_set(data_header.dataset)
+
+
+def test_crypt4gh_set_meta_with_bad_recrypt_header(c4gh_loader, c4gh_data_complete, c4gh_data_payload):
+    with c4gh_data_complete as data_complete:
+        c4gh_loader.set_meta(dataset=data_complete.dataset)
+
+        assert data_complete.dataset.metadata.crypt4gh_header
+        assert data_complete.dataset.metadata.crypt4gh_metadata_header_sha256
+        assert data_complete.dataset.metadata.crypt4gh_dataset_header_sha256
+        assert not data_complete.dataset.metadata.crypt4gh_compute_node_keypair_id
+
+        with c4gh_data_payload as data_payload:
+            with pytest.raises(ValueError):
+                c4gh_loader.set_meta(dataset=data_complete.dataset, crypt4gh_header=data_payload.contents)
+
+            _assert_c4gh_metadata_not_set(data_complete.dataset)
+
+
+def _assert_c4gh_metadata_not_set(dataset: DatasetProtocol):
+    assert not dataset.metadata.crypt4gh_header
+    assert not dataset.metadata.crypt4gh_metadata_header_sha256
+    assert not dataset.metadata.crypt4gh_dataset_header_sha256
+    assert not dataset.metadata.crypt4gh_compute_node_keypair_id
